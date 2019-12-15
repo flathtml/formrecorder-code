8cf411859168a9dbccef1244921c795951226321
Dayanand B Menashi <dayamenashi@gmail.com>
2019-12-14 22:39:19 -0500

    Add files via upload

---
+++ simplerecordergoogle/simplerecorder-msg.php
@@ -0,0 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$content = ' 
+ <h5 align="center"><i>Audio Assignments made easy</i></h5>
+       <h6 align="center">Our Customized Audio Forms have a built-in audio recorder that results in higher accuracy and productivity.</h6>
+      <div align="center"><a href="https://www.formrecorder.com/contact.html" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Contact us for a FREE Trial Form</a></div>     
+    ';
+ echo $content;
+?>
+ 
